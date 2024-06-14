5badf2e5ea08ace7cb0df3b47973b0aa52f5b00f
BaditSad <bdumortier0@gmail.com>
2024-06-14 13:25:58 +0200

    docker env correction

--- server.js
+++ server.js
@@ -2,25 +2,11 @@ require('dotenv').config();
 
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 3010;
-const mongoURI = process.env.MONGO_URI;
 
 app.use(cors());
-
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log("Connected to MongoDB!");
-})
-.catch((err) => {
-  console.error("Error connecting to MongoDB:", err);
-  process.exit();
-});
 
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
